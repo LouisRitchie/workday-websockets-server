f6e3e11632866f5c877cca932177a7d78430f9a2
Louis Ritchie <louiscritchie@gmail.com>
2016-11-13 22:16:45 -0800

    :fire: the server code

---
+++ README.md
@@ -0,0 +1,9 @@
+# startup-slam
+Slides, code, etc. for http://startupslam.io
+
+## How to Setup The Socket Server
+1. cd socket-server
+2. brew update (if need be)
+3. brew install node
+4. npm install
+5. node .
---
+++ index.js
@@ -0,0 +1,79 @@
+const io = require('socket.io')();
+const fs = require('fs');
+
+let mediaPath = __dirname + '/../d3-audio-spectrum/public/media';
+let servePath = 'media/';
+let playlist  = [];
+let playing   = null;
+let voteDelay = 250; // 1/4 second delay between votes
+
+// generate playlist from files in the media path
+for (file of fs.readdirSync(mediaPath)) {
+  let parts = file.split('-');
+  if (parts.length !== 2) {
+    console.log('Ignoring: ', file);
+    continue;
+  }
+
+  playlist.push({
+    file:   servePath + file,
+    title:  parts[1].substring(0, parts[1].lastIndexOf('.')).trim(),
+    artist: parts[0].trim(),
+    votes:  0
+  });
+
+  playlist.sort((a, b) => {
+    return a.file.toLowerCase().localeCompare(b.file.toLowerCase());
+  });
+}
+
+io.on('connection', (socket) => {
+  // emit current/initial state
+  socket.emit('playlist-update', playlist);
+  if (playing) socket.emit('play-start', playing);
+
+  // broadcast audio spectrum data
+  socket.on('audio-update-private', (data) => {
+    socket.broadcast.emit('audio-update', data);
+  });
+
+  // accept votes and update playlist
+  let lastVote = null;
+  socket.on('vote-cast', (song) => {
+    // throttle votes per client
+    let now = Date.now();
+    if (now - lastVote < voteDelay) return;
+    lastVote = now;
+
+    song = playlist.find((entry) => {
+      return entry.file === song.file;
+    });
+    if (!song) return;
+
+    song.votes++;
+
+    // emit update to all connected sockets
+    io.sockets.emit('playlist-update', playlist);
+  });
+
+  // listen for play start and update now playing
+  socket.on('play-start-private', (song) => {
+    song = playlist.find((entry) => {
+      return entry.file === song.file;
+    });
+    if (!song) return;
+
+    // update currently playing song
+    playing = song;
+
+    // we want to reset votes
+    song.votes = 0;
+
+    // update all connected sockets
+    io.sockets.emit('playlist-update', playlist);
+    io.sockets.emit('play-start', song);
+  });
+});
+
+console.log('Listening: 8080');
+io.listen(8080);
