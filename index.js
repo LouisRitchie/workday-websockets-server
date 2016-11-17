12cfb932ca66466f6ae2c7a440886a6515992feb
Louis Ritchie <louiscritchie@gmail.com>
2016-11-17 01:05:05 -0800

    add a ton of useful logs.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+"use strict";
 const io = require('socket.io')();
 const fs = require('fs');
 
 let mediaPath = __dirname + '/../d3-audio-spectrum/public/media';
+console.log(mediaPath);
 let servePath = 'media/';
 let playlist  = [];
 let playing   = null;
 let voteDelay = 250; // 1/4 second delay between votes
 
 // generate playlist from files in the media path
-for (file of fs.readdirSync(mediaPath)) {
+for (let file of fs.readdirSync(mediaPath)) {
   let parts = file.split('-');
   if (parts.length !== 2) {
     console.log('Ignoring: ', file);
@@ -28,18 +30,23 @@ for (file of fs.readdirSync(mediaPath)) {
 }
 
 io.on('connection', (socket) => {
+  console.log('connection')
   // emit current/initial state
+  console.log('emitting playlist-update')
   socket.emit('playlist-update', playlist);
   if (playing) socket.emit('play-start', playing);
 
   // broadcast audio spectrum data
   socket.on('audio-update-private', (data) => {
-    socket.broadcast.emit('audio-update', data);
+    console.log('Received audio-update-private')
+    console.log('Emitting audio-update')
+    socket.emit('audio-update', data);
   });
 
   // accept votes and update playlist
   let lastVote = null;
   socket.on('vote-cast', (song) => {
+	console.log('Received vote-cast')
     // throttle votes per client
     let now = Date.now();
     if (now - lastVote < voteDelay) return;
@@ -53,14 +60,21 @@ io.on('connection', (socket) => {
     song.votes++;
 
     // emit update to all connected sockets
+	console.log('Emitting vote-cast')
     io.sockets.emit('playlist-update', playlist);
   });
 
   // listen for play start and update now playing
   socket.on('play-start-private', (song) => {
-    song = playlist.find((entry) => {
-      return entry.file === song.file;
-    });
+    console.log('Received play-start-private')
+    if (song == false) {
+      song = playlist[0];
+    }
+    else {
+      song = playlist.find((entry) => {
+        return entry.file === song.file;
+      });
+    }
     if (!song) return;
 
     // update currently playing song
@@ -70,6 +84,8 @@ io.on('connection', (socket) => {
     song.votes = 0;
 
     // update all connected sockets
+    console.log('Emitting playlist-update')
+    console.log('Emitting play-start')
     io.sockets.emit('playlist-update', playlist);
     io.sockets.emit('play-start', song);
   });
